3ce2c32406b66356d2646643accd95a10b29bc94
Netohbk <ernstpm.92@gmail.com>
2022-09-19 09:12:17 -0500

    Se sube paquete Calculadora con evaluaciónPostfija

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Ernesto Palma
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EvaluacionPostfija ev = new EvaluacionPostfija();
+        //System.out.println(ev.evaluaPost("489-541-/+8*+705-/4*-"));
+        //System.out.println(ev.evaluaPost("489-541-/+8*+75/4*-"));
+        //System.out.println(ev.evaluaPost("(4.9)89-541-/+8*+75/4*-"));
+        //System.out.println(ev.evaluaPost("(8.5)(7.2)(5.3)+(89.5)(8.5)/-*"));
+        System.out.println(ev.evaluaPost("(8.5)(7)(5)+(89.5)(8)/-*"));
+        //System.out.println(ev.evaluaPost("(8.8985)(7.2)(5.3)+(89.5)(8.5)/-*"));
+        
+    }
+    
+}
---
+++ Calculadora/src/calculadora/EvaluacionPostfija.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+import pilas.PilaA;
+import pilas.PilaADT;
+
+/**
+ *
+ * @author Ernesto Palma
+ * * Definiciòn de la clase Evaluación de Postfija que usa pilas
+ */
+public class EvaluacionPostfija <T>{
+    
+    /**
+     * Constructor por omisión
+     */
+    public EvaluacionPostfija(){
+        
+    }
+    
+    /**
+     * Método que regresa el resultado de une expresión en postfija usando pilas
+     * @param cadena
+     * @return double
+     */
+    public double evaluaPost(String cadena){
+        PilaADT<Double> pila=new PilaA();
+        int i=0;
+        double resul, numer;
+        while(i<cadena.length()){
+            System.out.println("Valor de i en inicio: "+i);
+            if(this.operador(cadena.charAt(i))){
+                numer=pila.pop();
+                switch(cadena.charAt(i)){
+                    case'+': resul=pila.pop() + numer;
+                            pila.push(resul);
+                            break;
+                    case'-': resul=pila.pop() - numer;
+                            pila.push(resul);
+                            break;
+                    case'*': resul=pila.pop() * numer;
+                            pila.push(resul);
+                            break;
+                    case'/': resul=pila.pop() / numer;
+                            pila.push(resul);
+                            break;
+                    case'^': resul=Math.pow(pila.pop(), numer);
+                            pila.push(resul);
+                            break;
+                }
+            }
+            else if(this.parentesis(cadena.charAt(i))){
+                StringBuilder sb=new StringBuilder();
+                int cont=0;
+                while(!this.parentesis(cadena.charAt(i+1))){
+                    sb.append(cadena.charAt(i+1));
+                    i++;
+                    cont++;
+                }
+                pila.push(Double.parseDouble(sb.toString()));
+                i++;
+                System.out.println("valor de i"+i);
+            }    
+            else
+                pila.push(Double.parseDouble(cadena.substring(i, i+1)));  
+            i++;
+        }
+        return pila.peek();
+    }
+    
+    /**
+     * Método que indica si un caracter de una cadena de texto es un operador 
+     * @param cadena
+     * @return boolean
+     */
+    public boolean operador(char cadena){
+        boolean resp=false;
+        if(cadena == '+' | cadena == '-' | cadena == '*' | cadena == '/' | cadena == '^')
+            resp=true;
+        return resp;
+    }
+    
+    /**
+     * Método que indica si un caracter de una cadena de texto es un paréntesis
+     * @param cadena
+     * @return 
+     */
+    public boolean parentesis(char cadena){
+        boolean resp=false;
+        if(cadena == '(' | cadena== ')')
+            resp = true;
+        return resp;
+    }
+    
+}
